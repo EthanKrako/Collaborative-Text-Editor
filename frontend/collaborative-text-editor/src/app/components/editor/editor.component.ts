c0e31ff51a64cef3679789c6a4e07f29c8ae58b1
EthanKrako <77197427+EthanKrako@users.noreply.github.com>
2026-03-02 18:01:28 +0100

    reactive text storage using RxJS

--- frontend/collaborative-text-editor/src/app/components/editor/editor.component.ts
+++ frontend/collaborative-text-editor/src/app/components/editor/editor.component.ts
@@ -1,6 +1,7 @@
-import { Component } from "@angular/core";
-import { FormsModule } from "@angular/forms";
+import { Component, inject, OnInit } from "@angular/core";
+import { FormsModule, ValueChangeEvent } from "@angular/forms";
 import { QuillModule } from "ngx-quill";
+import { EditorStoreService } from "../../services/EditorStore/editor-store.service";
 
 @Component({
     selector: 'app-editor',
@@ -9,6 +10,18 @@ import { QuillModule } from "ngx-quill";
     imports: [FormsModule, QuillModule]
 })
 
-export class Editor {
+export class Editor implements OnInit {
+    editorStoreService = inject(EditorStoreService);
     editorContent: string = '';
+
+    ngOnInit(): void {
+        this.editorStoreService.contentSubjectObservable.subscribe(value => {
+            this.editorContent = value;
+        })
+    }
+
+    onContentChanged(newContent: string) {
+        this.editorStoreService.setContent(newContent);
+        console.log('Content updated in store:', this.editorStoreService.getContent());
+    }
 }
