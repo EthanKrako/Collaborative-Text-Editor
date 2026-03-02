3c8b80a0a746f067968ae685beba850e24ed67d2
EthanKrako <77197427+EthanKrako@users.noreply.github.com>
2026-03-02 17:13:07 +0100

    added quill editor

--- frontend/collaborative-text-editor/src/app/components/editor/editor.component.ts
+++ frontend/collaborative-text-editor/src/app/components/editor/editor.component.ts
@@ -1,9 +1,14 @@
 import { Component } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { QuillModule } from "ngx-quill";
 
 @Component({
     selector: 'app-editor',
     templateUrl: 'editor.component.html',
     styleUrl: 'editor.component.css',
+    imports: [FormsModule, QuillModule]
 })
 
-export class Editor {}
+export class Editor {
+    editorContent: string = '';
+}
