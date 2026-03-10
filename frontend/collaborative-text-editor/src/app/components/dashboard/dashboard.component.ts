ecaa6f6ea57e57eb3cf6fabb5e114e936140a011
EthanKrako <77197427+EthanKrako@users.noreply.github.com>
2026-03-10 18:44:16 +0100

    fix async loop error

--- frontend/collaborative-text-editor/src/app/components/dashboard/dashboard.component.ts
+++ frontend/collaborative-text-editor/src/app/components/dashboard/dashboard.component.ts
@@ -1,11 +1,13 @@
 import { Component, inject } from "@angular/core";
 import { Router } from "@angular/router";
+import { AsyncPipe } from "@angular/common";
 import { DocumentStoreService } from "../../services/DocumentStore/document-store.service";
 
 @Component({
     selector: 'dashboard',
     templateUrl: 'dashboard.component.html',
     styleUrl: 'dashboard.component.css',
+    imports: [AsyncPipe]
 })
 export class Dashboard{
     private router = inject(Router);
