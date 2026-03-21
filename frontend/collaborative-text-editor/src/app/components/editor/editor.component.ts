d3c8cce7e866fc2d782643706efe721b5f34cdb5
EthanKrako <77197427+EthanKrako@users.noreply.github.com>
2026-03-21 13:57:43 +0100

    Yjs setup on frontend

--- frontend/collaborative-text-editor/src/app/components/editor/editor.component.ts
+++ frontend/collaborative-text-editor/src/app/components/editor/editor.component.ts
@@ -2,6 +2,7 @@ import { Component, inject, OnInit } from "@angular/core";
 import { FormsModule, ValueChangeEvent } from "@angular/forms";
 import { QuillModule } from "ngx-quill";
 import { DocumentStoreService } from "../../services/DocumentStore/document-store.service";
+import * as Y from 'yjs';
 
 @Component({
     selector: 'app-editor',
@@ -14,21 +15,47 @@ export class Editor implements OnInit {
     documentStoreService = inject(DocumentStoreService);
     editorContent: string = '';
     documentTitle: string = 'Title';
-    documentId: string = ''
+    documentId: string = '';
+
+    ydoc: Y.Doc | null = null;
+    ytext: Y.Text | null = null;
+    yjsObserver: ((event: Y.YTextEvent) => void) | null = null;
 
     ngOnInit(): void {
         this.documentStoreService.activeDocument$.subscribe(doc => {
             if (doc) {
-                this.editorContent = doc.content;
                 this.documentTitle = doc.title;
                 this.documentId = doc.id;
-            }
+                this.ydoc = new Y.Doc();
+                this.ytext = this.ydoc.getText('quill-content');
+
+                if (this.ytext.length === 0 && doc.content) {
+                    this.ytext.insert(0, doc.content);
+                }
+                if (this.yjsObserver) {
+                    this.ytext.unobserve(this.yjsObserver);
+                }
+                this.yjsObserver = (event: Y.YTextEvent) => {
+                    const newContent = this.ytext!.toString();
+                    if (this.editorContent !== newContent) {
+                        this.editorContent = newContent;
+                    }
+                };
+                this.ytext.observe(this.yjsObserver);
+
+                this.editorContent = this.ytext.toString();
+                }
         })
     }
 
     onContentChanged(newContent: string) {
-        if (this.documentId) {
-            this.documentStoreService.updateContent(this.documentId, newContent);
+        if (this.ytext && newContent !== this.ytext.toString()) {
+            this.ytext.delete(0, this.ytext.length);
+            this.ytext.insert(0, newContent);
+
+            if (this.documentId) {
+                this.documentStoreService.updateContent(this.documentId, newContent);
+            }
         }
     }
 }
